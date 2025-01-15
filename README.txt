70609fec1c14c2160783cd0389cae4d4f43087e9
rachel9786 <145170526+rachel9786@users.noreply.github.com>
2025-01-14 19:34:40 -0500

    beginnings

---
+++ README.txt
@@ -0,0 +1,6 @@
+
+
+This website is published on CMU's UserWeb service
+
+to publish updates from this www folder go to the following URL:
+    https://www.andrew.cmu.edu/server/publish.html
---
+++ index_script.js
@@ -0,0 +1,9 @@
+
+function displayHomePage() {
+	//document.getElementById("projects").style.display = "block";
+	location.href = "personal_website/home_page.html";
+}
+
+function displayConsoleWebapp() {
+	location.href = "S25_build18_webapp/homepage.html";
+}
